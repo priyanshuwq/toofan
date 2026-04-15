62eda6afc6d9d20bbcf93f4bb2d8a7eb34334c2c
itniuma <itniuma@proton.me>
2026-04-16 05:20:51 +0800

    Fix #19: Add toofan cli command for version check  (#20)
    * fix: Add toofan cli command for version check 
    
    Add a --version flag to the toofan CLI that prints the current version
    
    ---------
    
    Co-authored-by: Amit <theamit.969@gmail.com>

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 
@@ -8,7 +9,17 @@ import (
 	"github.com/vyrx-dev/toofan/internal/tui"
 )
 
+var version = "dev"
+
 func main() {
+	v := flag.Bool("version", false, "Print current version")
+	flag.Parse()
+
+	if *v {
+		fmt.Println("toofan " + version)
+		os.Exit(0)
+	}
+
 	p := tea.NewProgram(tui.New(), tea.WithAltScreen())
 	if _, err := p.Run(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
